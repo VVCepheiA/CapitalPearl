aac7b24a37ea64c177baab93cf69707839da0bd0
VVCepheiA <oyevera@gmail.com>
2015-03-22 05:31:09 -0400

    add front end and working dc

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
+var Donation = require('../models/donation');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Capital Pearl' });
+});
+
+router.get('/impact', function(req, res, next) {
+  Donation.find(function(err, donations) {
+    if (err) {
+      return res.send(err);
+    }
+    res.render('impact', { donations: donations });
+    // res.json(donations);
+  });
+  
 });
 
 module.exports = router;
